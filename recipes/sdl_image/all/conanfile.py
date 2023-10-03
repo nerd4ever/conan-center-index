09ad864056a4597f573e6566ed409baa43f523b9
Sileno Brito <silenobrito@gmail.com>
2023-10-03 17:23:18 -0300

    downgrade from sdl custom

--- recipes/sdl_image/all/conanfile.py
+++ recipes/sdl_image/all/conanfile.py
@@ -94,7 +94,7 @@ class SDLImageConan(ConanFile):
 
     def requirements(self):
         # Headers are exposed https://github.com/conan-io/conan-center-index/pull/16167#issuecomment-1508347351
-        self.requires("sdl/2.28.2@nerd4ever", transitive_headers=True)
+        self.requires("sdl/2.26.5@nerd4ever", transitive_headers=True)
         if self.options.with_libtiff:
             self.requires("libtiff/4.6.0")
         if self.options.with_libjpeg:
